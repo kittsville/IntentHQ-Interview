617b9bc419ba17c297e16709568f447b32689094
Kit <kittsville@googlemail.com>
2018-01-07 17:15:37 +0000

    Count horizontally adjacent water as single pond
    #4

--- pondsizes/pondsizes.py
+++ pondsizes/pondsizes.py
@@ -1,7 +1,24 @@
 def getPondSizes(waterSquares):
     ponds = []
+    unexploredWater = set(waterSquares)
 
-    for water in waterSquares:
-        ponds.append(1)
+    while len(unexploredWater) > 0:
+        waterPosition = unexploredWater.pop()
 
+        ponds.append(countAdjacentWater(waterPosition, unexploredWater))
     return ponds
+
+def countAdjacentWater(position, unexploredWater):
+    xPos = position[0]
+    yPos = position[1]
+
+    waterCount = 1
+
+    adjacentSquares = [(xPos + 1, yPos), (xPos - 1, yPos)]
+
+    for square in adjacentSquares:
+        if square in unexploredWater:
+            unexploredWater.remove(square)
+            waterCount += countAdjacentWater(square, unexploredWater)
+
+    return waterCount
--- pondsizes/test.py
+++ pondsizes/test.py
@@ -19,3 +19,15 @@ class TestPondSizes(unittest.TestCase):
             pondsizes.getPondSizes([(0, 0), (1, 2)]),
             [1, 1]
         )
+
+    def testHorizontallyAdjacentWaterPond(self):
+        self.assertItemsEqual(
+            pondsizes.getPondSizes([(0, 0), (1, 0)]),
+            [2]
+        )
+
+    def testMultipleHorizontallyAdjacentWaterPonds(self):
+        self.assertItemsEqual(
+            pondsizes.getPondSizes([(0, 0), (1, 0), (1, 2), (2, 2), (3, 2)]),
+            [2, 3]
+        )
